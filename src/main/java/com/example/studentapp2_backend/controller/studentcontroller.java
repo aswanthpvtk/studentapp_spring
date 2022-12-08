4235d5e41278421be37ea742d3b279268b5647c5
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-08 15:44:17 +0530

    stud

--- src/main/java/com/example/studentapp2_backend/controller/studentcontroller.java
+++ src/main/java/com/example/studentapp2_backend/controller/studentcontroller.java
@@ -1,33 +1,60 @@
 package com.example.studentapp2_backend.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.studentapp2_backend.dao.StudentDao;
+import com.example.studentapp2_backend.model.Students;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class studentcontroller {
+
+
+    @Autowired
+    private StudentDao dao;
+
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String Homepage()
     {
-        return "welcom to my web page";
+        return "welcom to my web my my page";
     }
 
-    @GetMapping("/contact")
-   public String Contact()
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
+    public Map<String,String> PatientAdd(@RequestBody Students p)
     {
-        return "welcom to my contact page";
+
+        System.out.println(p.getName().toString());
+        dao.save(p);
+        HashMap<String,String> map =new HashMap<>();
+        map.put("status","success");
+        return map;
+
     }
 
-    @GetMapping("/gallary")
-    public String Gallery()
+    @CrossOrigin(origins = "*")
+    @GetMapping("/v")
+    public List<Students> View()
     {
-        return "welcom to my gallary page";
+        return (List<Students>) dao.findAll();
     }
 
-    @GetMapping("/home")
-    public String Home()
+
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search",consumes = "application/json",produces = "application/json")
+    public List<Students> Search(@RequestBody Students s)
     {
-        return "welcom to my home page";
+
+        String admno=String.valueOf(s.getAdmno());
+
+        return(List<Students>)dao.SearchStudent(s.getAdmno());
     }
+
 
 
 
